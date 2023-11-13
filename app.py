7272a3606816764fee71f8a587410c891e502187
Javier Ramirez <96668930+Jabonsote@users.noreply.github.com>
2023-11-12 22:27:47 -0800

    Remove background fun

--- app.py
+++ app.py
@@ -1,9 +1,41 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from PIL import Image, ImageOps
+from rembg import remove
 import sqlite3
+import os
 
 app = Flask(__name__)
 
-# Path: /
+
+ 
+
 @app.route('/')
 def index():
-    return "Bienvenido al sistemin!"
+    return render_template('base.html')
+
+
+@app.route('/procesar', methods=['POST'])
+def procesar_imagen():
+    if 'imagen' in request.files:
+        imagen = request.files['imagen']
+        if imagen.filename != '':
+            # Guarda la imagen cargada en un archivo temporal
+            temp_path = 'temp.png'
+            imagen.save(temp_path)
+
+            # Procesa la imagen para eliminar el fondo
+            input_image = Image.open(temp_path)
+            output_image = remove(input_image)
+
+            # Guarda la imagen procesada en la carpeta 'static'
+            output_path = 'static/imagen_procesada.png'
+            output_image.save(output_path)
+
+            # Limpia el archivo temporal
+            os.remove(temp_path)
+
+            return render_template('resultado.html')
+    return "No se ha cargado una imagen válida."
+
+if __name__ == '__main__':
+    app.run()
