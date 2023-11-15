3e62cb2f6f9895b37a54f1feb4b4b7130c6dea6d
asaPUP <asael.silvam@gmail.com>
2023-11-15 12:33:48 -0800

    Funciones aparte

--- functions.py
+++ functions.py
@@ -1,53 +1,34 @@
-from flask import Flask, render_template, request, redirect, url_for
-from PIL import Image, ImageOps
-from rembg import remove
 import sqlite3
 import os
-from functions import get_backgrounds, get_gallery, generate_image
+from PIL import Image, ImageOps
+from rembg import remove
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 database_path = os.path.join(current_dir, 'database/sistemin.db')
 
-app = Flask(__name__)
+# Funcion para obtener las imagenes de background de la base de datos en una tupa (...) de tuplas (id, path)
+def get_backgrounds():
+    # Obtiene los fondos de la base de datos
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM background')
+    backgrounds = cursor.fetchall()
+    conn.close()
 
-# Configuración de la carpeta de subida de archivos
-app.config['UPLOAD_FOLDER'] = os.path.join(current_dir, 'static/uploads')
+    return backgrounds # backgrounds es una tupla de tuplas (id, path)
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
+def get_gallery():
+    # Obtiene las ultimas 20 imagenes generadas de la base de datos
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM generated ORDER BY id DESC LIMIT 20')
+    gallery = cursor.fetchall()
+    conn.close()
 
-@app.route('/crear')
-def crear_imagen():
-    backgrounds = get_backgrounds()
-    return render_template('backgrounds.html', backgrounds=backgrounds)
+    return gallery # gallery es una tupla de tuplas (id, path)
 
-@app.route('/galeria')
-def galeria():
-    gallery = get_gallery()
-    return render_template('galeria.html', gallery=gallery)
-
-@app.route('/procesar/<int:background_id>')
-def procesar_imagen(background_id):
-    if background_id == None:
-        return "No se ha seleccionado un fondo válido."
-    
-    return render_template('process_image.html', background_id=background_id)
-
-@app.route('/procesar-imagen', methods=['POST'])
-def _procesar_imagen():
-    if 'imagen' not in request.files:
-        return "No se ha cargado una imagen válida."
-    
-    imagen = request.files['imagen']
-
-    if imagen.filename == '':
-        return "No se ha cargado una imagen válida."
-    
-    uploaded_id = generate_image(app, request, imagen)
-    
-    """
-    # Asigna un nombre único a la imagen cargada, obtenido del id en la base de datos
+def generate_image(app, request, imagen):
+        # Asigna un nombre único a la imagen cargada, obtenido del id en la base de datos
     conn = sqlite3.connect(database_path)
     cursor = conn.cursor()
     cursor.execute('SELECT MAX(id) FROM generated')
@@ -100,23 +81,5 @@ def _procesar_imagen():
 
     # Limpia el archivo temporal
     os.remove(temp_path)
-    """
 
-    return redirect(url_for('resultado', id=uploaded_id))
-
-@app.route('/resultado/<int:id>')
-def resultado(id):
-    conn = sqlite3.connect(database_path)
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM generated WHERE id = ?', (id,))
-    image = cursor.fetchone()
-    conn.close()
-
-    if image == None:
-        return "No se ha encontrado la imagen solicitada."
-    
-    return render_template('resultado.html', image=image) # image es una tupla con los datos de la imagen (id, relative_path)
-
-# Para iniciar el servidor de pruebas, usar:
-#  $ flask run
-#  $ flask --debug run (para iniciarlo en modo de depuración)
+    return uploaded_id
